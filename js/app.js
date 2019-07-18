232f2c85e8e0b88184e3b9ddb690e586585462f2
Calvin G Hall <123chall@gmail.com>
2019-07-17 20:26:07 -0500

    Finished game Still a hard lack of front end, but this project is mostly about the implementing the game anyway.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 (() => {
   'use strict';
-  const PLAYER_NONE = 0;
-  const PLAYER_ONE = 1; //Black
-  const PLAYER_TWO = 2; //White
+  const NONE = 'none';
+  const BLACK = 'black';
+  const WHITE = 'white';
+  const DIRECTIONS = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
 
   let boardState = [[]];
-  let currentPlayer = 0;
+  let currentPlayer = BLACK;
+  let move = 0;
 
   function putPiece(x, y) {
-    console.log('Doing put piece');
-    if (boardState[y][x] !== PLAYER_NONE) {
+    if (boardState[y][x] !== NONE) {
       return; //That piece is already placed.
     }
     //Flip pieces in each of the 8 directions, only if the adjacent piece is not owned.
-    let directions = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
     let found = false;
-    directions.forEach((direction) => {
+    DIRECTIONS.forEach((direction) => {
       if (flipPieces(x, y, direction[0], direction[1])) {
         found = true;
       }
@@ -24,47 +24,87 @@
       return;
     }
     boardState[y][x] = currentPlayer;
-    if (currentPlayer === PLAYER_ONE) {
-      currentPlayer = PLAYER_TWO;
-    } else {
-      currentPlayer = PLAYER_ONE;
+    updatePiece(x, y);
+    move++;
+    if (move === 60 || (!hasMove(BLACK) && !hasMove(WHITE))) { //End conditions.
+      let s = score();
+      if (s.black > s.white) {
+        alert(`Black wins, ${s.black} to ${s.white}!`);
+      } else if (s.black === s.white) {
+        alert('It\'s a tie!');
+      } else {
+        alert(`White wins, ${s.white} to ${s.black}!`);
+      }
+      resetBoard();
+      return;
     }
-    updateBoard();
+    if (currentPlayer === BLACK && hasMove(WHITE)) {
+      currentPlayer = WHITE;
+      console.log('White\'s turn');
+    } else {
+      currentPlayer = BLACK;
+      console.log('Black\'s turn');
+    }
   }
 
-  function checkPath(player, startX, startY, dirX, dirY) {
-    let x = startX + dirX;
-    let y = startY + dirY;
-    if (boardState[y][x] === currentPlayer) {
-      return false;
-    }
-    for(; x >= 0 && y >= 0 && x < 8 && y < 8; x += dirX, y += dirY) {
-      if (boardState[y][x] === currentPlayer) {
-        return true;
+  function score() {
+    let black = 0;
+    let white = 0;
+    boardState.forEach((row) => {
+      row.forEach((state) => {
+        if (state === BLACK) {
+          black++;
+        }
+        if (state === WHITE) {
+          white++;
+        }
+      });
+    });
+    return {black: black, white: white};
+  }
+
+  function hasMove(player) {
+    let found = false;
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        if (boardState[y][x] !== NONE) {
+          continue;
+        }
+        DIRECTIONS.forEach((direction) => {
+          if (walkPath(player, x, y, direction[0], direction[1]).canWalk) {
+            found = true;
+          }
+        });
+        if (found) {
+          return true;
+        }
       }
     }
     return false;
   }
 
-  function flipPieces(startX, startY, dirX, dirY) {
-    let found = false;
-    //Beefiest for-loop ever...
+  function walkPath(player, startX, startY, dirX, dirY) {
     let x = startX + dirX;
     let y = startY + dirY;
-    if (boardState[y][x] === currentPlayer) {
-      return false;
+    if (x < 0 || y < 0 || x > 7 || y > 7 || boardState[y][x] === player) {
+      return { canWalk: false, x: x, y: y };
     }
-    for(; x >= 0 && y >= 0 && x < 8 && y < 8; x += dirX, y += dirY) {
-      if (boardState[y][x] === currentPlayer) {
-        found = true;
-        break;
+    for(; x >= 0 && y >= 0 && x < 8 && y < 8 && boardState[y][x] !== NONE; x += dirX, y += dirY) {
+      if (boardState[y][x] === player) {
+        return { canWalk: true, x: x, y: y };
       }
     }
-    if (!found) {
+    return { canWalk: false, x: x, y: y };
+  }
+
+  function flipPieces(startX, startY, dirX, dirY) {
+    let results = walkPath(currentPlayer, startX, startY, dirX, dirY);
+    if (!results.canWalk) {
       return false;
     }
-    for(; x !== startX || y !== startY; x -= dirX, y -= dirY) {
+    for(let x = results.x, y = results.y; x !== startX || y !== startY; x -= dirX, y -= dirY) {
       boardState[y][x] = currentPlayer;
+      updatePiece(x, y);
     }
     return true;
   }
@@ -74,10 +114,10 @@
     for (let y = 0; y < 8; y++) {
       boardState[y] = [];
       for (let x = 0; x < 8; x++) {
-        boardState[y][x] = PLAYER_NONE;
+        boardState[y][x] = NONE;
         let piece = document.createElement('div');
         piece.id = `piece-${x}-${y}`;
-        piece.classList.toggle('player-none', true);
+        piece.classList.toggle(NONE, true);
         piece.addEventListener('click', () => putPiece(x, y));
         parent.appendChild(piece);
       }
@@ -87,37 +127,28 @@
   function resetBoard() {
     for (let y = 0; y < 8; y++) {
       for (let x = 0; x < 8; x++) {
-        boardState[y][x] = PLAYER_NONE;
+        boardState[y][x] = NONE;
       }
     }
-    boardState[4][4] = boardState[3][3] = PLAYER_TWO;
-    boardState[4][3] = boardState[3][4] = PLAYER_ONE;
-    currentPlayer = PLAYER_ONE;
+    boardState[4][4] = boardState[3][3] = WHITE;
+    boardState[4][3] = boardState[3][4] = BLACK;
+    currentPlayer = BLACK;
+    move = 0;
+    updateBoard();
+  }
+
+  function updatePiece(x, y) {
+    let piece = document.getElementById(`piece-${x}-${y}`);
+    piece.classList.toggle(NONE, false);
+    piece.classList.toggle(BLACK, false);
+    piece.classList.toggle(WHITE, false);
+    piece.classList.toggle(boardState[y][x]);
   }
 
   function updateBoard() {
     for (let y = 0; y < 8; y++) {
       for (let x = 0; x < 8; x++) {
-        let piece = document.getElementById(`piece-${x}-${y}`);
-        switch (boardState[y][x]) {
-          case PLAYER_NONE:
-          piece.classList.toggle('player-none', true);
-          piece.classList.toggle('player-one', false);
-          piece.classList.toggle('player-two', false);
-          break;
-          case PLAYER_ONE:
-          piece.classList.toggle('player-none', false);
-          piece.classList.toggle('player-one', true);
-          piece.classList.toggle('player-two', false);
-          break;
-          case PLAYER_TWO:
-          piece.classList.toggle('player-none', false);
-          piece.classList.toggle('player-one', false);
-          piece.classList.toggle('player-two', true);
-          break;
-          default:
-          console.log('Unexpected board state: '+boardState[y][x]);
-        }
+        updatePiece(x, y);
       }
     }
   }
@@ -127,9 +158,7 @@
     let container = document.getElementById('othello-container');
     createBoard(container);
     resetBoard();
-    updateBoard();
   }
 
   setupBoard();
-
 })();
